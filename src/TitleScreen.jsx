8faed7f09968f2b1d62708b8ac30e8e2f8fdd2ba
crazyclownytr <yashtr02@gmail.com>
2025-06-10 11:11:46 +0200

    Titlescreen basics

--- src/TitleScreen.jsx
+++ src/TitleScreen.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const TitleScreen = () => (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-5xl font-bold mb-2">Welcome!</h1>
-        <p className="text-2xl text-gray-600">This is the title screen.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-background">
+        <h1 className="text-8xl font-klear text-white mb-2">GROENLANDIA</h1>
+
+
+        <div className="mt-8 flex flex-col items-center">
+            <button className="mt-4 px-6 py-2 bg-rozeButton text-white font-semibold rounded hover:bg-hoverButton">
+                Inloggen
+            </button>
+            <button className="mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600">
+                Aanmelden
+            </button>
+        </div>
     </div>
 );
 
