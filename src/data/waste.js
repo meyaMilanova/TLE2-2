be2e83429f7f8bd81bc283bdc677375d31307fd9
justin <kinhanjustin@gmail.com>
2025-06-12 13:05:47 +0200

    waste data added

---
+++ src/data/waste.js
@@ -0,0 +1,48 @@
+const wasteItems = [
+    {
+        name: "Afvalzak",
+        image: "/src/assets/images/wastesorting/afvalzak.png",
+        category: "restafval",
+    },
+    {
+        name: "Appel",
+        image: "/src/assets/images/wastesorting/appel.png",
+        category: "gft",
+    },
+    {
+        name: "Batterij",
+        image: "/src/assets/images/wastesorting/batterij.png",
+    },
+    {
+        name: "Broodje",
+        image: "/src/assets/images/wastesorting/broodje.png",
+        category: "gft",
+    },
+    {
+        name: "Flesje",
+        image: "/src/assets/images/wastesorting/flesje.png",
+        category: "plastic",
+    },
+    {
+        name: "Karton",
+        image: "/src/assets/images/wastesorting/karton.png",
+        category: "papier",
+    },
+    {
+        name: "Papier",
+        image: "/src/assets/images/wastesorting/papier.png",
+        category: "papier",
+    },
+    {
+        name: "Pizzadoos",
+        image: "/src/assets/images/wastesorting/pizzadoos.png",
+        category: "papier",
+    },
+    {
+        name: "Tasje",
+        image: "/src/assets/images/wastesorting/tasje.png",
+        category: "plastic",
+    },
+];
+
+export default wasteItems;
