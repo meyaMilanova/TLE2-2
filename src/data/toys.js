1ea59d053c07bcecb146b9767ea076dedbdf626f
justin <kinhanjustin@gmail.com>
2025-06-17 10:53:32 +0200

    Toys data change

---
+++ src/data/toys.js
@@ -0,0 +1,52 @@
+const toys = [
+    {
+        id: 1,
+        name: "Katbat",
+        image: "/images/toys/katbat-grey.png",
+        plastic: 10,
+        paper: 20,
+        status: "Gekocht"
+    },
+    {
+        id: 2,
+        name: "Blauwe Katbat",
+        image: "/images/toys/katbat-blue.png",
+        plastic: 10,
+        paper: 20,
+        status: "Kopen"
+    },
+    {
+        id: 3,
+        name: "Roze Katbat",
+        image: "/images/toys/katbat-pink.png",
+        plastic: 10,
+        paper: 20,
+        status: "Kopen"
+    },
+    {
+        id: 4,
+        name: "Plant-tank",
+        image: "/images/toys/tank-green.png",
+        plastic: 10,
+        paper: 20,
+        status: "Actief"
+    },
+    {
+        id: 5,
+        name: "Roos-tank",
+        image: "/images/toys/tank-red.png",
+        plastic: 10,
+        paper: 20,
+        status: "Kopen"
+    },
+    {
+        id: 6,
+        name: "Iris-tank",
+        image: "/images/toys/tank-blue.png",
+        plastic: 10,
+        paper: 20,
+        status: "Kopen"
+    }
+];
+
+export default toys;
