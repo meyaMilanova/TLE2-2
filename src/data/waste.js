3fc7c7f1a121cbc47533205edf76866edcc12c88
justin <kinhanjustin@gmail.com>
2025-06-23 14:14:17 +0200

    data change

--- src/data/waste.js
+++ src/data/waste.js
@@ -121,7 +121,7 @@ export const facts = {
         "Wist je dat kauwgom wel 20 jaar op straat kan blijven plakken?",
         "Gooi kauwgom altijd in de prullenbak, anders blijft het overal kleven."
     ],
-    "Pizzadoos (vuil)": [
+    "Vuile pizzadoos": [
         "Vette pizzadozen horen bij het restafval. Vet papier kan niet worden hergebruikt.",
         "Probeer de schone stukken af te scheuren en die wel bij het papier te doen.",
         "Wist je dat vet karton machines verstopt in de papierfabriek?"
