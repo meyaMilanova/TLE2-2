1615f1a815f6a3399d035c17bd9d1f2f42e052c5
Pascal1400 <pascalberens14@gmail.com>
2025-06-23 11:24:03 +0200

    BackButtonProfiel gefixt

---
+++ src/components/BackButtonProfiel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function BackButtonProfiel() {
+    const navigate = useNavigate();
+
+    return (
+        <button
+            className="absolute uppercase top-6 left-6 bg-orange-500 rounded-full w-16 h-16 flex items-center justify-center text-white text-3xl z-50"
+            onClick={() => navigate(-1)}
+            type="button"
+        >
+            <span className="font-bold">&#8592;</span>
+        </button>
+    );
+}
+
+export default BackButtonProfiel;
