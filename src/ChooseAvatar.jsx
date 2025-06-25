9f2cc104181ddfb178773b526bd512218f69225d
Pascal1400 <pascalberens14@gmail.com>
2025-06-25 10:13:23 +0200

    BackButtonProfiel bug gefixt

--- src/ChooseAvatar.jsx
+++ src/ChooseAvatar.jsx
@@ -44,7 +44,7 @@ function AvatarSelection() {
 
             if (response.ok) {
                 localStorage.setItem('selectedAvatar', selectedAvatar);
-                navigate('/hoofdpagina'); // Navigate to homepage
+                navigate(-1); // Navigate back to homepage
             } else {
                 const errorData = await response.json();
                 console.error('Error saving avatar:', errorData);
